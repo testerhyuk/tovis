958bca52503433dfcaf225cd220bcdb635e22bfd
Hyuk <testerhyuk214@gmail.com>
2025-03-03 19:40:17 +0900

    test code delete

--- worklog/src/test/java/com/tovis/worklog/api/WorkApiTest.java
+++ worklog/src/test/java/com/tovis/worklog/api/WorkApiTest.java
@@ -11,39 +11,39 @@ import org.springframework.web.client.RestClient;
 import java.util.List;
 
 public class WorkApiTest {
-    RestClient restClient = RestClient.create("http://localhost:8080");
-
-    @Test
-    void createTest() {
-        WorkResponse response = create(new WorkRequest(
-                "김규혁", 300
-        ));
-
-        System.out.println("response = " + response);
-    }
-
-    WorkResponse create(WorkRequest request) {
-        return restClient.post()
-                .uri("/v1/worklogs")
-                .body(request)
-                .retrieve()
-                .body(WorkResponse.class);
-    }
-
-    @Test
-    void readAllTest() {
-        List<WorkResponse> response = restClient.get()
-                .uri("/v1/datas")
-                .retrieve()
-                .body(new ParameterizedTypeReference<List<WorkResponse>>() {});
-
-        System.out.println("datas = " + response);
-    }
-
-    @Getter
-    @AllArgsConstructor
-    static class WorkRequest {
-        private String worker;
-        private int quantity;
-    }
+//    RestClient restClient = RestClient.create("http://localhost:8080");
+//
+//    @Test
+//    void createTest() {
+//        WorkResponse response = create(new WorkRequest(
+//                "김규혁", 300
+//        ));
+//
+//        System.out.println("response = " + response);
+//    }
+//
+//    WorkResponse create(WorkRequest request) {
+//        return restClient.post()
+//                .uri("/v1/worklogs")
+//                .body(request)
+//                .retrieve()
+//                .body(WorkResponse.class);
+//    }
+//
+//    @Test
+//    void readAllTest() {
+//        List<WorkResponse> response = restClient.get()
+//                .uri("/v1/datas")
+//                .retrieve()
+//                .body(new ParameterizedTypeReference<List<WorkResponse>>() {});
+//
+//        System.out.println("datas = " + response);
+//    }
+//
+//    @Getter
+//    @AllArgsConstructor
+//    static class WorkRequest {
+//        private String worker;
+//        private int quantity;
+//    }
 }
